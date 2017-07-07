f4516e3ab2a3572818086c0a8194f8cae6f26b85
Stuart Grimes <stuartgrimes@me.com>
2017-07-07 16:24:21 +0100

    inputting signup form now raises an exception if email address provided is already in database

---
+++ app/Validation/Exceptions/EmailAvailableException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stuart
+ * Date: 07/07/2017
+ * Time: 16:05
+ */
+
+namespace App\Validation\Exceptions;
+
+
+class EmailAvailableException
+{
+
+}
---
+++ app/Validation/Rules/EmailAvailable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stuart
+ * Date: 07/07/2017
+ * Time: 15:28
+ */
+
+namespace App\Validation\Rules;
+
+
+class EmailAvailable
+{
+
+}
